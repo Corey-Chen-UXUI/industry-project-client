6ba81f221c923de6fff9a57b8889af249b5ffb77
Anna L <annalin@Annas-MacBook-Pro.local>
2023-07-21 02:58:54 -0400

    Adjusted file/folder structure to closer mimic pages from design flow. Rough styling framework added.

---
+++ src/Components/NavigationLanding/NavigationLanding.jsx
@@ -0,0 +1,10 @@
+import "./NavigationLanding.scss";
+
+const NavigationLanding = () => {
+  return (
+    <div className="navigation-landing">
+    </div>
+  )
+}
+
+export default NavigationLanding
